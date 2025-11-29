058846cb9fcc29af7f84752400f9b5272f72e841
jhonkeithman123 <keithvirgenes17@gmail.com>
2025-11-29 22:29:59 +0800

    Updated commit for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/digital-portfolio-system-client/",
+  base: process.env.VITE_BASE || "/",
   plugins: [react()],
 });
